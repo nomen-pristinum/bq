fa4228ebe4b621c236dd3a28a678773368294292
nikola <leka.rankovic@gmail.com>
2023-09-04 05:35:46 +0200

    Switched to SummingMergeTree

--- Deltas.sql
+++ Deltas.sql
@@ -3,8 +3,8 @@ CREATE TABLE ethereum.aggregated_deltas
     tx_date Date,
     address_bin String,
     currency_id UInt32,
-    balance AggregateFunction(sum, Float64)
-) ENGINE = AggregatingMergeTree()
+    balance_delta Float64
+) ENGINE = SummingMergeTree(balance_delta)
 PARTITION BY toYYYYMM(tx_date)
 ORDER BY (tx_date, address_bin, currency_id);
 
@@ -15,11 +15,11 @@ SELECT
     tx_date,
     address_bin,
     currency_id,
-    sumState(value) as balance
-FROM ethereum.balance_deltas
-GROUP BY tx_date, address_bin, currency_id;
+    value as balance_delta
+FROM ethereum.balance_deltas;
+--GROUP BY tx_date, address_bin, currency_id;
 
-CREATE VIEW ethereum.aggregated_deltas_vw
+/*CREATE VIEW ethereum.aggregated_deltas_vw
 AS
 SELECT
     tx_date,
@@ -27,4 +27,24 @@ SELECT
     currency_id,
     sumMerge(balance) as balance_delta
 FROM ethereum.aggregated_deltas
-GROUP BY tx_date, address_bin, currency_id;
+GROUP BY tx_date, address_bin, currency_id;*/
+
+CREATE TABLE ethereum.aggregated_deltas_2 --replica to enable efficient cumulative JOINs
+(
+    tx_date Date,
+    address_bin String,
+    currency_id UInt32,
+    balance Float64
+) ENGINE = SummingMergeTree(balance)
+PARTITION BY toYYYYMM(tx_date)
+ORDER BY (tx_date, address_bin, currency_id);
+
+CREATE MATERIALIZED VIEW ethereum.aggregated_deltas_2_mv
+TO ethereum.aggregated_deltas
+AS
+SELECT
+    tx_date,
+    address_bin,
+    currency_id,
+    value as balance
+FROM ethereum.balance_deltas;
