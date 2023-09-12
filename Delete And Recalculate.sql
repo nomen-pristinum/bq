9e3badab1f53acb244817cf72370ee639670eb22
nikola <leka.rankovic@gmail.com>
2023-09-12 18:11:29 +0200

    - fixed formatting and currency_id - removed obsolete code for aggregated_deltas_2 - added stored current balances

--- Delete And Recalculate.sql
+++ Delete And Recalculate.sql
@@ -1,129 +1,108 @@
---TO-DO
---currency ID
---remove aggregated deltas 2
---deal with current balances
-select * from discrepancy_log;
 
-delete from balance_deltas
-where address_bin in (
-    select distinct address_bin
-    from discrepancy_log
-    where fixed_flag = 0
-    and block = next_block_to_fix)
-and tx_time >= (
-    select distinct block_time
-    from discrepancy_log
-    where fixed_flag = 0
-    and block = next_block_to_fix
-    limit 1 --safety
+SELECT * FROM discrepancy_log;
+
+DELETE FROM balance_deltas
+WHERE (address_bin, currency_id) IN (
+    SELECT distinct address_bin, currency_id
+    FROM discrepancy_log
+    WHERE fixed_flag = 0
+    AND block = next_block_to_fix)
+AND tx_time >= (
+    SELECT distinct block_time
+    FROM discrepancy_log
+    WHERE fixed_flag = 0
+    AND block = next_block_to_fix
+    LIMIT 1 --safety
     ) ;
 
-delete from aggregated_deltas
-where address_bin in (
-    select distinct address_bin
-    from discrepancy_log
-    where fixed_flag = 0
-    and block = next_block_to_fix)
-and tx_date = (select toDate(block_time)
-    from discrepancy_log
-    where fixed_flag = 0
-    and block = next_block_to_fix);
+DELETE FROM aggregated_deltas
+WHERE (address_bin, currency_id) IN (
+    SELECT distinct address_bin, currency_id
+    FROM discrepancy_log
+    WHERE fixed_flag = 0
+    AND block = next_block_to_fix)
+AND tx_date = (SELECT toDate(block_time)
+    FROM discrepancy_log
+    WHERE fixed_flag = 0
+    AND block = next_block_to_fix);
 
-insert into ethereum.aggregated_deltas
+INSERT INTO ethereum.aggregated_deltas
 SELECT
     tx_date,
     address_bin,
     currency_id,
     value as balance_delta
 FROM ethereum.balance_deltas
-where address_bin in (
-    select distinct address_bin
-    from discrepancy_log
-    where fixed_flag = 0
-    and block = next_block_to_fix)
-and tx_date = (select toDate(block_time)
-    from discrepancy_log
-    where fixed_flag = 0
-    and block = next_block_to_fix);
+WHERE (address_bin, currency_id) IN (
+    SELECT distinct address_bin, currency_id
+    FROM discrepancy_log
+    WHERE fixed_flag = 0
+    AND block = next_block_to_fix)
+AND tx_date = (SELECT toDate(block_time)
+    FROM discrepancy_log
+    WHERE fixed_flag = 0
+    AND block = next_block_to_fix);
 
-delete from aggregated_deltas_2
-where address_bin in (
-    select distinct address_bin
-    from discrepancy_log
-    where fixed_flag = 0
-    and block = next_block_to_fix)
-and tx_date = (select toDate(block_time)
-    from discrepancy_log
-    where fixed_flag = 0
-    and block = next_block_to_fix);
+TRUNCATE TABLE current_balances;
 
-insert into ethereum.aggregated_deltas_2
+INSERT INTO current_balances
 SELECT
-    tx_date,
     address_bin,
     currency_id,
-    value as balance_delta
-FROM ethereum.balance_deltas
-where address_bin in (
-    select distinct address_bin
-    from discrepancy_log
-    where fixed_flag = 0
-    and block = next_block_to_fix)
-and (select toDate(block_time)
-    from discrepancy_log
-    where fixed_flag = 0
-    and block = next_block_to_fix);
+    sum(balance_delta) as balance
+FROM aggregated_deltas
+GROUP BY address_bin, currency_id;
 
-delete from cumulative_balances_history
-where address_bin in (
-    select distinct address_bin
-    from discrepancy_log
-    where fixed_flag = 0
-    and block = next_block_to_fix)
-and tx_date >=  -- everything is polluted
+DELETE FROM cumulative_balances_history
+WHERE (address_bin, currency_id) IN (
+    SELECT distinct address_bin, currency_id
+    FROM discrepancy_log
+    WHERE fixed_flag = 0
+    AND block = next_block_to_fix)
+AND tx_date >=  -- everything is polluted
                 -- for the affected addresses
                 -- past this point in time:
-    (select toDate(block_time)
-    from discrepancy_log
-    where fixed_flag = 0
-    and block = next_block_to_fix);
+    (SELECT toDate(block_time)
+    FROM discrepancy_log
+    WHERE fixed_flag = 0
+    AND block = next_block_to_fix);
 
 INSERT INTO cumulative_balances_history
 SELECT
     d1.tx_date,
     d1.address_bin,
     d1.currency_id,
-    sum(d2.balance_delta) as cumulative_balance
+    sum(d2.balance_delta) AS cumulative_balance
 FROM ethereum.aggregated_deltas d1
 JOIN ethereum.aggregated_deltas d2
 ON d1.address_bin = d2.address_bin
 AND d1.currency_id = d2.currency_id
 WHERE d2.tx_date <= d1.tx_date
 AND d1.tx_date >= (
-    select toDate(block_time)
-    from discrepancy_log
-    where fixed_flag = 0
-    and block = next_block_to_fix)
-AND d1.address_bin in (
-    select distinct address_bin
-    from discrepancy_log
-    where fixed_flag = 0
-    and block = next_block_to_fix)
+    SELECT toDate(block_time)
+    FROM discrepancy_log
+    WHERE fixed_flag = 0
+    AND block = next_block_to_fix)
+AND (d1.address_bin, d1.currency_id) IN (
+    SELECT distinct address_bin, currency_id
+    FROM discrepancy_log
+    WHERE fixed_flag = 0
+    AND block = next_block_to_fix)
 GROUP BY d1.tx_date, d1.address_bin, d1.currency_id
 ORDER BY d1.address_bin, d1.tx_date;
 
 
-alter table discrepancy_log
-update fixed_flag = 1
-where block = next_block_to_fix;
+ALTER TABLE discrepancy_log
+UPDATE fixed_flag = 1
+WHERE block = next_block_to_fix;
 
-alter table discrepancy_log
-update next_block_to_fix = (
-    select min(block)
-    from discrepancy_log
+ALTER TABLE discrepancy_log
+UPDATE next_block_to_fix = (
+    SELECT min(block)
+    FROM discrepancy_log
     WHERE fixed_flag = 0
 )
-where 1 = 1;
+WHERE 1 = 1;
 
 
 
