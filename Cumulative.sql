0f63db8bb777bbb34f5d8ede4a196c03e6d4b35b
nikola <leka.rankovic@gmail.com>
2023-09-12 07:12:30 +0200

    - introduced stored current balances - cumulative history is now a ReplacingMergeTree - Logic filling the history now split into regular operations (current data) and historical data fix MVs

--- Cumulative.sql
+++ Cumulative.sql
@@ -1,47 +1,54 @@
 --TO-DO
 --SummingMT current balance table
---MV inserting into it from agg deltas WHERE date = today()
---INSERT to initialise it
---VW pulling from the table final SUM()
---MV inserting its changes into Cumulative History table
+CREATE TABLE current_balances
+(
+    address_bin String,
+    currency_id UInt32,
+    balance Float64
+) ENGINE = SummingMergeTree(balance)
+ORDER BY (address_bin, currency_id);
+
+--INSERT to initialise current balances
+INSERT INTO current_balances
+SELECT
+    address_bin,
+    currency_id,
+    sum(balance_delta) as balance
+FROM aggregated_deltas
+GROUP BY address_bin, currency_id;
+
+--MV inserting into it FROM balance deltas WHERE date = today()
+CREATE MATERIALIZED VIEW current_balances_mv
+to current_balances
+as
+SELECT
+    address_bin,
+    currency_id,
+    value as balance
+FROM balance_deltas
+WHERE tx_date = today();
+
+--VW pulling the final SUM() FROM balances
+CREATE VIEW current_balances_vw
+AS
+SELECT
+    address_bin,
+    currency_id,
+    sum(balance) as balance
+FROM current_balances
+GROUP BY address_bin, currency_id;
+
+CREATE VIEW top_holders_vw
+as
+SELECT *
+FROM current_balances
+ORDER BY balance desc
+LIMIT 100;
+
 --Cumulative History becomes ReplacingMT
 --stop using aggregated delta 2, not needed with this approach
---do use finalising SUM() when pulling from aggregations
+--do use finalising SUM() when pulling FROM aggregated deltas
 --all this to handle regular business
---another MV for historical data fixes
---  WHERE < today(),
---  Cumulative History JOIN Changes
---  USING date, currency, address
---  insert change+old value
-
-CREATE VIEW daily_balances AS
-SELECT
-    address_bin,
-    currency_id,
-    sum(balance_delta) as cumulative_balance
-FROM  aggregated_deltas
-WHERE aggregated_deltas.tx_date <= today() -- protection against polluted data
-GROUP BY address_bin, currency_id;
-
-
-SELECT
-    address_bin,
-    currency_id,
-    sum(balance_delta) as cumulative_balance
-FROM  aggregated_deltas
-WHERE aggregated_deltas.tx_date <= '2015-12-12'-- desired date here
-GROUP BY address_bin, currency_id;
-
-SELECT
-    address_bin,
-    currency_id,
-    sum((balance_delta)) as cumulative_balance
-FROM  aggregated_deltas
-WHERE aggregated_deltas.tx_date <= today() -- desired date here
-GROUP BY address_bin, currency_id
-ORDER BY cumulative_balance desc
-LIMIT 10;
-
 CREATE TABLE ethereum.cumulative_balances_history
 (
     tx_date Date,
@@ -51,69 +58,102 @@ CREATE TABLE ethereum.cumulative_balances_history
 ) ENGINE = ReplacingMergeTree(cumulative_balance)
 ORDER BY (tx_date, address_bin, currency_id);
 
+--MV inserting the changes of current balances into Cumulative History table
+CREATE MATERIALIZED VIEW cumulative_balances_history_mv
+TO cumulative_balances_history AS
+SELECT *
+FROM current_balances;
+
+-- another MV for historical data fixes
+--  WHERE < today(),
+--  Cumulative History JOIN Changes
+--  USING date, currency, address
+--  insert change+old value*
+CREATE MATERIALIZED VIEW cumulative_balances_history_fix_mv
+TO cumulative_balances_history
+AS
+SELECT tx_date,
+       address_bin,
+       currency_id,
+       sum(cumulative_balance/cnt + daily_change)
+           as cumulative_balance
+FROM (
+    SELECT tx_date,
+        address_bin,
+        currency_id,
+        cumulative_balance,
+        daily_change,
+        count(*) OVER (
+            PARTITION BY tx_date, address_bin, currency_id)
+            AS cnt
+    FROM (
+        SELECT tx_date,
+            address_bin,
+            currency_id,
+            sum(value) as daily_change
+        FROM ethereum.balance_deltas
+        WHERE tx_date < today()
+        GROUP BY tx_date, address_bin, currency_id) ch
+        --changes only, trigger
+    JOIN cumulative_balances_history h
+    ON ch.address_bin = h.address_bin
+    AND ch.currency_id = h.currency_id
+    WHERE ch.tx_date <= h.tx_date
+)
+GROUP BY tx_date, address_bin, currency_id;
+
+
 /* initial load, run if there is existing data in transfers_tx_storage */
 INSERT INTO cumulative_balances_history
 SELECT
     d1.tx_date,
     d1.address_bin,
     d1.currency_id,
-    sum(d2.balance_delta) as cumulative_balance
-FROM (
+    sum(d2.balance_delta) AS cumulative_balance
+FROM ( --surprisingly small performance loss here:
     SELECT
         tx_date,
         address_bin,
         currency_id,
-        sum(balance_delta) as cumulative_balance
+        sum(balance_delta) as balance_delta
     FROM ethereum.aggregated_deltas
-    group by tx_date, address_bin, currency_id) d1
-    JOIN ethereum.aggregated_deltas d2
-                    ON d1.address_bin = d2.address_bin
-                        AND d1.currency_id = d2.currency_id
-      WHERE d2.tx_date <= d1.tx_date d1
-JOIN ethereum.aggregated_deltas d2
+    GROUP BY tx_date, address_bin, currency_id) d1
+JOIN (
+    SELECT
+        tx_date,
+        address_bin,
+        currency_id,
+        sum(balance_delta) as balance_delta
+    FROM ethereum.aggregated_deltas
+    GROUP BY tx_date, address_bin, currency_id) d2
 ON d1.address_bin = d2.address_bin
 AND d1.currency_id = d2.currency_id
 WHERE d2.tx_date <= d1.tx_date
 GROUP BY d1.tx_date, d1.address_bin, d1.currency_id
 ORDER BY d1.tx_date, d1.address_bin, d1.currency_id;
 
-CREATE MATERIALIZED VIEW ethereum.cumulative_balances_history_mv
-TO ethereum.cumulative_balances_history AS
-SELECT
-    d1.tx_date,
-    d1.address_bin,
-    d1.currency_id,
-    sum(d2.balance_delta) as cumulative_balance
-FROM ethereum.aggregated_deltas d1 -- changes only, trigger
-JOIN ethereum.aggregated_deltas_2 d2 -- full change history, no trigger
-ON d1.address_bin = d2.address_bin
-AND d1.currency_id = d2.currency_id
-WHERE d2.tx_date <= d1.tx_date
-GROUP BY d1.tx_date, d1.address_bin, d1.currency_id;
-
-
 
 CREATE VIEW ethereum.cumulative_balances_history_vw
 AS
-select *
-from (
+SELECT *
+FROM (
     SELECT tx_date,
          address_bin,
          currency_id,
          cumulative_balance,
-         max(tx_date) over (
-             partition by address_bin, currency_id)
-             as last_balance_date
-    FROM (
-        select tx_date,
+         max(tx_date) OVER (
+             PARTITION BY address_bin, currency_id)
+             AS last_balance_date
+    FROM cumulative_balances_history FINAL /*(
+        SELECT tx_date,
                address_bin,
                currency_id,
                sum(cumulative_balance) as cumulative_balance
-        from cumulative_balances_history
-        group by tx_date, address_bin, currency_id)
-    where tx_date <= today()
+        FROM cumulative_balances_history
+        GROUP BY tx_date, address_bin, currency_id)*/
+    WHERE tx_date <= today()
 )
-where tx_date = last_balance_date
+WHERE tx_date = last_balance_date
 ;
 
 
